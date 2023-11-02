64e42bd807957760e2b47b3b1e9771a762f8ba62
Gaspard Merten <gaspard.mp.work@gmail.com>
2023-11-02 15:50:05 +0100

    Adding aggregated speed for STIB

---
+++ components/stib/harvesters/aggregated_speed.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+from src.components import Harvester
+
+
+class StibSegmentsAggregatedSpeedHarvester(Harvester):
+    def run(self, sources):
+        sources = list(([
+            row[2]
+            for row in sources
+        ]))
+        sources_flat = []
+
+        for source in sources:
+            sources_flat.extend(source)
+
+        df = pd.DataFrame(sources_flat)
+
+        if df.empty:
+            return []
+
+        # Merge on pointId, lineId and directionId and average speed
+        df = df.groupby(["pointId", "lineId", "directionId"]).mean()
+        # Reset index
+        df = df.reset_index()
+
+        return df.to_dict(orient="records")
