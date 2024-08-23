17e3027669905519898676c50f9984207f465893
Gaspard Merten <gaspard@norse.be>
2024-08-23 09:43:26 +0200

    Fixing stib vehicle identify

--- components/stib/harvesters/identify_vehicle/identify_vehicle.py
+++ components/stib/harvesters/identify_vehicle/identify_vehicle.py
@@ -31,8 +31,10 @@ class STIBVehicleIdentifyHarvester(Harvester):
             data_df = gpd.GeoDataFrame()
             # Merge the latest 10 data with the current batch
             data_df = pd.concat([data_df, latest_10_data_with_uuid_df])
-            data_df.set_crs(epsg=4326, inplace=True)
-
+            if not data_df.empty:
+                data_df.set_crs(epsg=4326, inplace=True)
+            else:
+                data_df = gpd.GeoDataFrame()
             # Load each to GeoDataFrame and concat
             for item in query[i: i + DATAPOINT_PER_BATCH]:
                 timestamp = item.date.timestamp()
