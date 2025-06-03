47a6f8482017a6b5500350c939542103a27cc607
CePseudo <hoffmann.axel.6032@gmail.com>
2025-06-03 19:53:41 +0200

    change de lijn api from v2 to v3

--- components/de_lijn/collectors/gtfs_realtime.py
+++ components/de_lijn/collectors/gtfs_realtime.py
@@ -8,7 +8,7 @@ from src.components import Collector
 class DeLijnGTFSRealtimeCollector(Collector):
     def run(self):
         endpoint = (
-            "https://api.delijn.be/gtfs/v2/realtime?json=false&delay=true&canceled=true"
+            "https://api.delijn.be/gtfs/v3/realtime?json=false&delay=true&canceled=true"
         )
         response_data = requests.get(
             endpoint, headers={"Ocp-Apim-Subscription-Key": os.environ["DE_LIJN_API_KEY"]}
